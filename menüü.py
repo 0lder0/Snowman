68008259fc38aa610872f14ff4216480df29ae70
Estellio <94623286+Estellio@users.noreply.github.com>
2022-03-08 13:38:12 +0200

    Add files via upload

---
+++ menüü.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://192.168.22.172/menu-example/'
+sisu = requests.get(url).text
+tekst = BeautifulSoup(sisu, 'html.parser')
+fail = tekst.find_all('h2')
+
+lug = 0
+for a in fail:
+    lug += 1
+#lugerist läheb 1 maha, kuna see arvestab ka koolilõunatoetuse lehe all
+luger = lug - 1
+
+for luger in range(luger):
+    tags = tekst.find_all('h2')[luger]
+    try:
+        for i in tags:
+            print(i)
+            break
+        print('Lisainfo: ' + tags.small.string)
+        print('Hind: ' + tags.span.string + '\n')
+    except:
+        print('Lisainfo puudub')
+        print('Hind: ' + tags.span.string + '\n')
