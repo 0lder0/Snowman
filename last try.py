38bc558d72a58895123b17b872607d64e5783377
Estellio <94623286+Estellio@users.noreply.github.com>
2022-03-09 11:04:23 +0200

    Add files via upload

--- last try.py
+++ last try.py
@@ -1,18 +1,24 @@
 # impordime vajalikud moodulid
 from bs4 import BeautifulSoup
 import requests
+import json
 
 # avame ja loeme sisse veebilehe andmed
-url = 'https://siseveeb.voco.ee/veebivormid/sookla_menuu'
+url = 'http://192.168.22.172/menu-example/'
 sisu = requests.get(url).text
 tekst = BeautifulSoup(sisu, 'html.parser')
-# filtreerime vajaliku koodijupi
+# filtreerime vajalikud koodijupid
 fail = tekst.find_all('h2')
+thing = tekst.find_all('ul')
 
-# tühjad järjendid ja sõnastik
+# tühjad järjendid, sõnastik ja lugerid
+elementide_arv = []
 pealkirjad = []
+final_list = []
 json_list = []
 json_dict = {}
+food_dict ={}
+indeks = 0
 lug = 0
 
 # leiame veebilehelt toitude liigid
@@ -35,8 +41,8 @@ for luger in range(luger):
     # luger võtab ükshaaval elementide (toitude) koode
     tags = tekst.find_all('h2')[luger]
     # saame toidu nimetuse
-    for i in tags:
-        nimi = i
+    for toit in tags:
+        nimi = toit
         break
     # saame toidu lisainfo
     info = tags.small.string
@@ -46,9 +52,38 @@ for luger in range(luger):
         info = 'Lisainfo puudub'
     else:
         info = tags.small.string
-    # leiame hinna
-    hind = tags.span.string
+    #leiame hinna
+    hind1 = tags.span.string
+    hind = str(hind1).strip('€')
     # lisame andmed sõnastikku
-    json_dict = {'Nimi': nimi, 'Lisainfo': info, 'Hind': hind}
+    json_dict = {'nimetus': nimi, 'hind': hind, 'lisainfo': info}
     # lisame sõnastiku järjendisse
     json_list.append(json_dict)
+
+for i in range(5):
+    things = thing[i]
+    luger1 = 0
+    asi1 = '€'
+    for asi1 in things:
+        luger1 += 1
+    indeks += 1
+    if indeks == 5:
+        elementide_arv[-1] = elementide_arv[-1] + luger1
+    else:
+        elementide_arv.append(luger1)
+    
+xx = []
+for i in range(len(pealkirjad)):
+    food_dict = {'nimetus': pealkirjad[i], 'toidud': []}
+    for x in elementide_arv:
+        for l in range(x):
+            food_dict['toidud'].append(json_list[0])
+            json_list.pop(0)
+        elementide_arv.pop(0)
+        break
+    final_list.append(food_dict)
+
+print(final_list)
+with open('json_fail.py', 'w') as j_fail:
+    json.dump(final_list, j_fail, indent = 4)
+j_fail.close()
